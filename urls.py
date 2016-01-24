99f721b14d71f4480d7868ed1da79bdd57762f18
willwagner602 <willwagner602@gmail.com>
2016-01-23 23:02:09 -0500

    Fixed non-linux compatible pathname

--- urls.py
+++ urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name="LandingPage\home.html"), ),
+    url(r'^$', TemplateView.as_view(template_name="LandingPage/home.html"), ),
 ]
